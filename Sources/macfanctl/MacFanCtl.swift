04b2ef9aef84525281c6b78aebe356ab8ea5d4c2
2dubu <lgw101142@gmail.com>
2026-06-23 13:50:23 +0900

    Bump version to 0.3.0

--- Sources/macfanctl/MacFanCtl.swift
+++ Sources/macfanctl/MacFanCtl.swift
@@ -5,7 +5,7 @@ struct MacFanCtl: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "macfanctl",
         abstract: "Control Mac fan speeds via SMC.",
-        version: "0.2.0",
+        version: "0.3.0",
         subcommands: [
             ListCommand.self,
             WatchCommand.self,
